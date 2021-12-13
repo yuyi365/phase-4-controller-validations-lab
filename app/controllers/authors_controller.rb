a6034f9ab4abf798298a48c6832243192ac9f14f
Yuyi <yuyi365@gmail.com>
2021-12-13 16:55:10 -0500

    commit tree ddb84cfaf5302c62b3b7bc739e5b5d1b4208d9b7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,33 @@
 class AuthorsController < ApplicationController
-  
-  def show
-    author = Author.find(params[:id])
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
+  wrap_parameters format: []
 
+  def show
+    author = find_author
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
   private
-  
+
+  def find_author
+    Author.find(params[:id])
+  end
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             errors: invalid.record.errors,
+           },
+           status: :unprocessable_entity
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,34 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
+  wrap_parameters format: []
 
   def show
-    post = Post.find(params[:id])
-    
+    post = find_post
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    post.update!(post_params)
     render json: post
   end
 
   private
 
+  def find_post
+    Post.find(params[:id])
+  end
+
   def post_params
     params.permit(:category, :content, :title)
   end
 
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             errors: invalid.record.errors,
+           },
+           status: :unprocessable_entity
+  end
 end
